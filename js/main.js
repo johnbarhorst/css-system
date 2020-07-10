a5e7968ff5b21ec7926fcccf15fdb9874c005926
johnbarhorst <johnbarhorst.dev@gmail.com>
2020-07-09 20:47:22 -0500

    fix flash to blue on change

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
 const themeSelect = document.getElementById('theme-select');
 const body = document.getElementById('theme-body');
 
-
-// Started to make a more resilient way of toggling theme classes.
-// Then I remembered this is way overengineering.
-// Left it here in case I look back at this one day.
-const themeNames = Array.from(themeSelect.options).map(option => option.value);
-
+let currentTheme;
 
 themeSelect.addEventListener('change', (e) => {
   const theme = e.target.value;
-  body.removeAttribute('class');
+  body.classList.remove(currentTheme);
   body.classList.add(theme);
-
+  return currentTheme = theme;
 });
